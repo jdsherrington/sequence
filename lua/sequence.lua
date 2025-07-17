71a44c150b2fcd75c00d9fb807ab055aaeb95d6e
Jordan Sherrington <mail@jdsherrington.com>
2025-07-18 02:59:16 +1000

    fix: bugfix

--- lua/sequence.lua
+++ lua/sequence.lua
@@ -6,8 +6,14 @@ local function get_nvim_lua_path()
 	local repo_root = source:match("(.*/)lua/sequence.lua$")
 	local nvim_lua = repo_root .. "nvim/lua/"
 	nvim_lua = vim.loop.fs_realpath(nvim_lua) or nvim_lua
+	if not nvim_lua:match("/$") then
+		nvim_lua = nvim_lua .. "/"
+	end
 	return nvim_lua
 end
+
 local nvim_lua = get_nvim_lua_path()
+
 package.path = nvim_lua .. "?.lua;" .. nvim_lua .. "?/init.lua;" .. package.path
+
 return require("sequence.init")
