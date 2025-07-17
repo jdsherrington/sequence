7f6cdebaeb1c51b48e758be76e17c712c2c3262d
Jordan Sherrington <mail@jdsherrington.com>
2025-07-18 02:44:00 +1000

    fix: bugfix

--- lua/sequence.lua
+++ lua/sequence.lua
@@ -5,4 +5,4 @@ local nvim_lua_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../
 if not vim.tbl_contains(vim.api.nvim_list_runtime_paths(), nvim_lua_path) then
 	vim.opt.rtp:append(nvim_lua_path)
 end
-return require("sequence")
+return require("sequence.init")
