b6a4c7b43279226057573503f53d119e4aa8853c
Jordan Sherrington <mail@jdsherrington.com>
2025-07-18 02:50:23 +1000

    fix: bugfix

--- lua/sequence.lua
+++ lua/sequence.lua
@@ -1,7 +1,6 @@
 -- sequence/lua/sequence.lua
 -- This shim adds the nvim/lua directory to the runtime path, then loads the theme
 
-local nvim_lua_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../../nvim/lua/?.lua"
-local nvim_lua_init_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../../nvim/lua/?/init.lua"
-package.path = nvim_lua_path .. ";" .. nvim_lua_init_path .. ";" .. package.path
+local base = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../../nvim/lua/"
+package.path = base .. "?.lua;" .. base .. "?/init.lua;" .. package.path
 return require("sequence")
