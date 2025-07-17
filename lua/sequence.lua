96e7db2aaae52e4abd0dab70386ff3a31f7b44f7
Jordan Sherrington <mail@jdsherrington.com>
2025-07-18 02:46:19 +1000

    fix: bugfix again

--- lua/sequence.lua
+++ lua/sequence.lua
@@ -1,7 +1,7 @@
 -- sequence/lua/sequence.lua
 -- This shim adds the nvim/lua directory to the runtime path, then loads the theme
 
-local nvim_lua_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../../nvim/lua"
+local nvim_lua_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "../../nvim"
 if not vim.tbl_contains(vim.api.nvim_list_runtime_paths(), nvim_lua_path) then
 	vim.opt.rtp:append(nvim_lua_path)
 end
